cc5492c0f550f200172bd7cf6de258141030d649
hanus <droidender@gmail.com>
2017-04-02 12:35:19 +0200

    display more info about hero

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,11 @@ export class Hero {
 
 @Component({
   selector: 'my-app',
-  template: `<h1>{{title}}</h1>
+  template: `
+  <h1>{{title}}</h1>
   <h2>{{hero.name}} details!</h2>
+  <div><label>id: </label>{{hero.id}}</div>
+  <div><label>name: </label>{{hero.name}}</div>
   `,
 })
 
