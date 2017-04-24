f089f6536d18bd757cfc2c42f8161e0139e67294
Hanus Hasl <hasl@speag.com>
2017-04-24 10:34:32 +0200

    add hero-search support
    use observables instead of promises

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { DashboardComponent } from './dashboard.component';
 import { HeroesComponent } from './heroes.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroService } from './hero.service';
+import { HeroSearchComponent } from "./hero-search.component";
 @NgModule({
   imports: [
     BrowserModule,
@@ -24,6 +25,7 @@ import { HeroService } from './hero.service';
     DashboardComponent,
     HeroDetailComponent,
     HeroesComponent,
+    HeroSearchComponent
   ],
   providers: [HeroService],
   bootstrap: [AppComponent]
